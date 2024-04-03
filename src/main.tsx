a315de9096ee49264e012bb9667eaba2abe29194
Otto Alexander Robsahm <111810346+Otro1@users.noreply.github.com>
2024-04-03 12:22:26 +0200

    ny branch

--- src/main.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./modules/application/App";
-
-const root = ReactDOM.createRoot(document.getElementById("root")!); // ! -> To tell TS that the root element is there.
-
-root.render(<App />);
